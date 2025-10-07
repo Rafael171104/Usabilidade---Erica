58947e6c7c99d4bd4567359ba13da49de9469a95
Rafael de Carvalho Gomes <82329108@ulife.com.br>
2025-10-07 11:39:44 -0300

    feat: create a Lembretes project

---
+++ tela-lembretes-app-react/frontend/tela-lembrete-app/src/components/LembreteForm.js
@@ -0,0 +1,20 @@
+import CampoTexto from './CampoTexto';
+import Botao from './Botao';
+
+function LembreteForm({value, onChange, onSubmit}){
+    return (
+        <div>
+            <h2>Novo Lembrete</h2>
+            <CampoTexto value={value}
+            onChange={onChange}
+            placeholder="Digite um Lembrete Aqui"
+            />
+            
+            <Botao texto="Criar Card"
+            onClick={onSubmit}/>
+
+        </div>
+    )
+}
+
+export default LembreteForm;
